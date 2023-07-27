3ea43f9ad9686a11ce65fa7ad2a1deafbba5df09
Victoria Wasonga <victoria.wasonga@strathmore.edu>
2023-07-27 16:01:26 +0300

    references

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 console.log("Victoria ");
 
 // const person=require('./person');
-//  console.log(person)
-const Person=require('./person');
-const person1=new Person('Ella Atieno', 5)
-person1.greetings()
+// //  console.log(person)
+// const Person=require('./person');
+// const person1=new Person('Ella Atieno', 5)
+// person1.greetings()
+
+const Logger=require('./logger');
+
+const logger=new Logger();
+
+logger.on('message',(data)=>console.log('Caller Listener:',data));
+
+logger.log("Hello World");
